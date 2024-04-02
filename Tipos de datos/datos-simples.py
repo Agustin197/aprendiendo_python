0323c25dbfdd2ad04be22926e7600c7e9261a7f3
Agustín Ceballos <agustin.97.ceballos@gmail.com>
2024-04-02 00:10:39 -0300

    first commit

---
+++ Tipos de datos/datos-simples.py
@@ -0,0 +1,8 @@
+#Tipos de datos 
+
+"Agustin" #String
+40 #INT
+40.2 #FLOAT
+True
+False #boolean
+
---
+++ variables/variables.py
@@ -0,0 +1,23 @@
+#Variables 
+
+#espacios que se almacen en la memoria de nuestro programa.
+#primero se declaran y luego se definen
+
+numero = 10
+numero += 50
+numero -= 10
+
+dos = numero + 100
+print(dos)
+
+A = 160
+B = 230
+
+Resultado = A + B 
+print(Resultado)
+
+nombre = "Agustin"
+
+saludar = f"Hola {nombre} como andas"
+
+print(saludar)
