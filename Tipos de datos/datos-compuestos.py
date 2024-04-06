5cfc47f03674fab317dc0fe7e24d55b96c32f316
Agustín Ceballos <agustin.97.ceballos@gmail.com>
2024-04-05 21:06:48 -0300

    add datos compuestos

--- Tipos de datos/datos-compuestos.py
+++ Tipos de datos/datos-compuestos.py
@@ -1,3 +1,25 @@
-lista = ["Kylo Ren","Messi",True,1.79]
+#datos compuestos
 
-print(lista[3])
+#creando una lista (se pueden modificar)
+lista = ["Lionel Andres","Messi",True,1.79]
+
+#creando una tupla (no se pueden modificar)
+tupla = ("Lionel Andres","Messi",True,1.79) 
+
+#valido
+lista[2] = False
+
+#no valido
+#tupla[1] = "Ronaldo" 
+
+#creando un conjunto (set) - (no se accede a elementos por índice, no almacena datos duplicados)
+conjunto = {"Lionel Andres","Messi",True,1.79}
+print(conjunto)
+
+#crando un diccionario (dict) - (key : value)
+diccionario= {
+    "nombre": "Agustin",
+    "apellido": "Ceballos",
+    "altura": 179
+}
+
